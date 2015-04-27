da6c5f4cd76efb7cf3199ca9e91edef3bb759e71
wkmanire <williamkmanire@gmail.com>
2015-04-26 19:32:17 -0700

    Did all the work for Max

--- integrationtests/remotelawnmowerultra.py
+++ integrationtests/remotelawnmowerultra.py
@@ -1,91 +1,28 @@
 __author__ = 'Max'
 
+import re
+from StringIO import StringIO
+
 from taz.game import Game, Scene
-from re import compile, match
 
 
 class Player(object):
-    def __init__(self, name):
-        self.name = name
-        self.current_room = 0
-        self.inventory = []
 
-
-class CommandProcessor(object):
     def __init__(self):
-        pass
-
-    def process(self, player, room, command_text):
-        if self.is_room_backyard(room):
-            run_backyard_command(command_text, player)
-
-    def run_backyard_command(self, command_text, player):
-        if self.is_looking_at_tools(command_text):
-            player.inventory.append("Screw Driver")
-            return "You look through the pile of tools. That screw driver might be useful. You take the Screw Driver"
-        elif self.is_looking_at_gnomes(command_text):
-            return "The gnomes play soccer and argue over a foul, which the referee-gnome not gave to the red-faced gnome."
-
-    @staticmethod
-    def is_looking_at_gnomes(command_text):
-        a = compile("look\s*a?t?\s*gnomes?")
-        if a.match(command_text):
-            return True
-        return False
-
-    @staticmethod
-    def is_looking_at_tools(command_text):
-        a = compile("look\s*a?t?\s*p?i?l?e?\s*\w*\s*t?o?o?l?s?")
-        if a.match(command_text):
-            return True
-        return False
-
-    @staticmethod
-    def is_room_backyard():
-        return room == "Backyard"
+        self.room_id = None  # TODO: rename to room_name
 
 
-class BackyardScene(Scene):
-    def __init__(self):
-        self.title = "Backyard"
-        self.exits = {}
-        self.item = "Screw Driver"
-        self.objects_of_rooms = []
-        self.commands_of_rooms = []
+class RoomScene(Scene):
 
-    def create_commands(self):
-        self.commands_of_rooms.append("look at tools")
-        self.commands_of_rooms.append("look at gnomes")
-        self.commands_of_rooms.append("take tools")
-        self.commands_of_rooms.append("take gnomes")
-        self.commands_of_rooms.append("look west")
-        self.commands_of_rooms.append("look east")
-        self.commands_of_rooms.append("look north")
-        self.commands_of_rooms.append("look south")
-        self.commands_of_rooms.append("go west")
-        self.commands_of_rooms.append("go east")
-        self.commands_of_rooms.append("go north")
-        self.commands_of_rooms.append("go south")
-
-    def create_exits(self):
-        self.exits["north"] = "Living Room"
-        self.exits["south"] = "Storage Shed"
-        self.exits["west"] = "Well, there is a big stone wall in front of you. No chance you are getting through that."
-        self.exits["east"] = "You can't go there. Your neighbor is just waiting for an opportunity to get a hold of you."
-
-    def create_objects(self):
-        self.objects_of_rooms.append("Pile of Tools")
-        self.objects_of_rooms.append("Gnomes")
-
-    def update(self, update_context):
-        pass
+    def __init__(self, identifier):
+        super(RoomScene, self).__init__(identifier)
+        self.output_buffer = None
+        self.player = None
+        self.commands = [(re.compile("^look$"), self.look_command),
+                         (re.compile("^look at (?P<what>.+)$"), self.look_at_command),
+                         (re.compile("^look (?P<what>.+)$"), self.look_at_command)]
 
     def initialize_scene(self):
-        print("You just stepped outside and are now standing on the backyard. " +
-              "You look around and see two things, well except that hugely overgrown lawn, that stick out. " +
-              "One is a Pile of Tools and the other is...a bunch of gnomes, which seem to argue?!")
-
-    def render(self, render_context):
         pass
 
     def resume(self):
@@ -94,293 +31,69 @@ class BackyardScene(Scene):
     def tear_down(self):
         pass
 
-
-class StorageShedScene(Scene):
-    def __init__(self):
-        self.title = "Storage Shed"
-        self.exits = {}
-        self.item = "Armadillos"
-        self.objectsOfRooms = []
-        self.commands_of_room = []
-
-    def create_exits(self):
-        self.exits["north"] = "Backyard"
-        self.exits["south"] = "You can't go there. There are only tools hanging on the wall."
-        self.exits["west"] = "You can't climb through that window."
-        self.exits["east"] = "You can't climb on the Grillmaster 3000"
-
-    def create_commands(self):
-        self.commands_of_room.append("look at hole")
-        self.commands_of_room.append("look at grillmaster")
-        self.commands_of_room.append("take armadillos")
-        self.commands_of_room.append("take grillmaster")
-        self.commands_of_room.append("look west")
-        self.commands_of_room.append("look east")
-        self.commands_of_room.append("look north")
-        self.commands_of_room.append("look south")
-        self.commands_of_room.append("go west")
-        self.commands_of_room.append("go east")
-        self.commands_of_room.append("go north")
-        self.commands_of_room.append("go south")
-
-    def create_objects(self):
-        self.objectsOfRooms.append("Hole in the ground")
-        self.objectsOfRooms.append("Grillmaster 3000")
-
     def update(self, update_context):
-        pass
+        self.output_buffer = StringIO()
+        commandText = update_context["input_fob"].readline().strip()
+        for expression, command in self.commands:
+            match = expression.match(commandText)
+            if match:
+                command(update_context, match)
+                break
+        self.output_buffer.seek(0)
 
-    def initialize_scene(self):
-        print("You entered the shed! Wow, this is a tiny shed. Even though there is a hole in the ground," +
-              "there is also the new Grillmaster 3000! That is the mothership of barbecue-grills.")
+    def look_command(self, update_context, match):
+        room = self.get_current_room(update_context)
+        self.output_buffer.write(room["description"])
+
+    def look_at_command(self, update_context, match):
+        room = self.get_current_room(update_context)
+        what = match.group("what")
+        obj = next((value for key, value in room["objects"].items() if key == what))
+        self.output_buffer.write(obj)
+
+    def get_current_room(self, update_context):
+        rooms = update_context["world_data"]["rooms"]
+        players_room_name = self.player.room_id
+        return next((r for r in rooms if r["name"] == players_room_name))
 
     def render(self, render_context):
+        output_fob = render_context["output_fob"]
+        for line in self.output_buffer:
+            output_fob.write(line)
+
+
+class GameFactory(object):
+
+    class MissingStartingRoom(Exception):
         pass
 
-    def resume(self):
-        pass
+    def __init__(self, input_fob, output_fob, world_data):
+        self.output_fob = output_fob
+        self.input_fob = input_fob
+        self.world_data = world_data
 
-    def tear_down(self):
-        pass
+    def create(self):
+        game = Game(self.get_update_context(), self.get_render_context())
+        room_scene = RoomScene("room")
+        game.register_new_scene(room_scene)
+        game.push_scene_on_stack("room")
+        room_scene.player = Player()
+        self.set_starting_room(room_scene.player)
+        return game
 
+    def get_update_context(self):
+        return {
+            "world_data": self.world_data,
+            "input_fob": self.input_fob
+        }
 
-class LivingRoomScene(Scene):
-    def __init__(self):
-        self.title = "Living Room"
-        self.exits = {}
-        self.item = "Antenna"
-        self.objectsOfRooms = []
+    def get_render_context(self):
+        return {
+            "output_fob": self.output_fob
+        }
 
-    def create_commands(self):
-        self.commands_of_room.append("look at wife")
-        self.commands_of_room.append("look at tv")
-        self.commands_of_room.append("look at n64")
-        self.commands_of_room.append("take tv")
-        self.commands_of_room.append("take n64")
-        self.commands_of_room.append("take wife")
-        self.commands_of_room.append("look west")
-        self.commands_of_room.append("look east")
-        self.commands_of_room.append("look north")
-        self.commands_of_room.append("look south")
-        self.commands_of_room.append("go west")
-        self.commands_of_room.append("go east")
-        self.commands_of_room.append("go north")
-        self.commands_of_room.append("go south")
-
-    def create_exits(self):
-        self.exits["north"] = "Kitchen"
-        self.exits["south"] = "Backyard"
-        self.exits["west"] = "Bedroom"
-        self.exits["east"] = "Your wife is sitting in front of the TV. If you don't want to through her and the TV out of the way and break through the wall behind, there is no chance of walking this way."
-
-    def create_objects(self):
-        self.objectsOfRooms.append("Wife")
-        self.objectsOfRooms.append("TV")
-        self.objectsOfRooms.append("N64")
-
-    def update(self, update_context):
-        pass
-
-    def initialize_scene(self):
-        print("You stepped into the living room. Your wife is eyeing you suspiciously, while the TV runs another episode of 'The Prince of Bel Air.'")
-
-    def render(self, render_context):
-        pass
-
-    def resume(self):
-        pass
-
-    def tear_down(self):
-        pass
-
-
-class KitchenScene(Scene):
-    def __init__(self):
-        self.title = "Kitchen"
-        self.exits = {}
-        self.item = "Battery"
-        self.objectsOfRooms = []
-
-    def create_commands(self):
-        self.commands_of_room.append("look at sink")
-        self.commands_of_room.append("look at microwave")
-        self.commands_of_room.append("take sink")
-        self.commands_of_room.append("take microwave")
-        self.commands_of_room.append("look west")
-        self.commands_of_room.append("look east")
-        self.commands_of_room.append("look north")
-        self.commands_of_room.append("look south")
-        self.commands_of_room.append("go west")
-        self.commands_of_room.append("go east")
-        self.commands_of_room.append("go north")
-        self.commands_of_room.append("go south")
-
-    def create_exits(self):
-        self.exits["north"] = "Garage"
-        self.exits["south"] = "Living Room"
-        self.exits["west"] = "Office"
-        self.exits["east"] = "There is a huge pile of dishes in front of that window, so you can definitely not go there."
-
-    def create_objects(self):
-        self.objectsOfRooms.append("Sink")
-        self.objectsOfRooms.append("Microwave")
-
-    def update(self, update_context):
-        pass
-
-    def initialize_scene(self):
-        print("You just entered the kitchen. You better get out of here quick, " +
-              "otherwise you might have to do all the dishes in the sink." +
-              "Get your food and RUN!")
-
-    def render(self, render_context):
-        pass
-
-    def resume(self):
-        pass
-
-    def tear_down(self):
-        pass
-
-
-class BedroomScene(Scene):
-    def __init__(self):
-        self.title = "Bedroom"
-        self.exits = {}
-        self.item = "N64 Controller"
-        self.objectsOfRooms = []
-
-    def create_commands(self):
-        self.commands_of_room.append("look at alarm clock")
-        self.commands_of_room.append("look at closet")
-        self.commands_of_room.append("take alarm clock")
-        self.commands_of_room.append("look west")
-        self.commands_of_room.append("look east")
-        self.commands_of_room.append("look north")
-        self.commands_of_room.append("look south")
-        self.commands_of_room.append("go west")
-        self.commands_of_room.append("go east")
-        self.commands_of_room.append("go north")
-        self.commands_of_room.append("go south")
-
-    def create_exits(self):
-        self.exits["north"] = "Garage"
-        self.exits["south"] = "You can't go there. There are only tools hanging on the wall."
-        self.exits["west"] = "Office"
-        self.exits["east"] = "You can't climb on the Grillmaster 3000"
-
-    def create_objects(self):
-        self.objectsOfRooms.append("Alarm Clock")
-        self.objectsOfRooms.append("Closet")
-
-    def update(self, update_context):
-        pass
-
-    def initialize_scene(self):
-        print("You just woke up and realize you are in the bedroom. That dream, though! You definitely need " +
-              "a new lawnmower. 'I will just build it myself...I got all the stuff at home. I better get going'." +
-              "But, you should get dressed first.")
-
-    def render(self, render_context):
-        pass
-
-    def resume(self):
-        pass
-
-    def tear_down(self):
-        pass
-
-
-class GarageScene(Scene):
-    def __init__(self):
-        self.title = "Garage"
-        self.exits = {}
-        self.item = "Duct Tape"
-        self.objectsOfRooms = []
-
-    def create_commands(self):
-        self.commands_of_room.append("look at car")
-        self.commands_of_room.append("look at ladder")
-        self.commands_of_room.append("take car")
-        self.commands_of_room.append("take ladder")
-        self.commands_of_room.append("look west")
-        self.commands_of_room.append("look east")
-        self.commands_of_room.append("look north")
-        self.commands_of_room.append("look south")
-        self.commands_of_room.append("go west")
-        self.commands_of_room.append("go east")
-        self.commands_of_room.append("go north")
-        self.commands_of_room.append("go south")
-
-    def create_exits(self):
-        self.exits["north"] = "You can't go on the streets in your stained boxers. Get dressed and shower you filth!"
-        self.exits["south"] = "Kitchen"
-        self.exits["west"] = "There's an awesome Audi A8 blocking your way....Stop staring, get back to work!"
-        self.exits["east"] = "There's a ladder hanging on the wall, but you can't climb it."
-
-    def create_objects(self):
-        self.objectsOfRooms.append("Car")
-        self.objectsOfRooms.append("Ladder")
-
-    def update(self, update_context):
-        pass
-
-    def initialize_scene(self):
-        print("You are in the garage now! There it is! Your eye-candy, your sweet-heart, your little cutie. The new Audi A8." +
-              "What a sight! But enough, that lawn is waiting for you to keep growing.")
-
-    def render(self, render_context):
-        pass
-
-    def resume(self):
-        pass
-
-    def tear_down(self):
-        pass
-
-
-class OfficeScene(Scene):
-    def __init__(self):
-        self.title = "Office"
-        self.exits = {}
-        self.item = "Hot Glue"
-        self.objectsOfRooms = []
-
-    def create_commands(self):
-        self.commands_of_room.append("look at computer")
-        self.commands_of_room.append("look at megan fox")
-        self.commands_of_room.append("take computer")
-        self.commands_of_room.append("take megan fox")
-        self.commands_of_room.append("look west")
-        self.commands_of_room.append("look east")
-        self.commands_of_room.append("look north")
-        self.commands_of_room.append("look south")
-        self.commands_of_room.append("go west")
-        self.commands_of_room.append("go east")
-        self.commands_of_room.append("go north")
-        self.commands_of_room.append("go south")
-
-    def create_exits(self):
-        self.exits["north"] = "There is a computer on there. 'Wow! Pygame2, I've been waiting for that for ages.' Now turn around!"
-        self.exits["south"] = "Bedroom"
-        self.exits["west"] = "There's a sexy picture of Megan Fox. But now is not the time to go there!"
-        self.exits["east"] = "Kitchen"
-
-    def create_objects(self):
-        self.objectsOfRooms.append("Computer")
-        self.objectsOfRooms.append("Megan Fox Poster")
-
-    def update(self, update_context):
-        pass
-
-    def initialize_scene(self):
-        print("You enter the office and your computer is still running. You look at the wall and get reminded why you better make sure your wife doesn't get into this room.")
-
-    def render(self, render_context):
-        pass
-
-    def resume(self):
-        pass
-
-    def tear_down(self):
-        pass
+    def set_starting_room(self, player):
+        try:
+            player.room_id = self.world_data["config"]["starting-room"]
+        except KeyError:
+            raise self.MissingStartingRoom()
--- integrationtests/test_remotelawnmowerultra.py
+++ integrationtests/test_remotelawnmowerultra.py
@@ -1,22 +1,140 @@
 __author__ = 'Max'
 
-from unittest import TestCase
-from integrationtests.remotelawnmowerultra import CommandProcessor
+from unittest import TestCase, skip
+import json
+from StringIO import StringIO
+
+from remotelawnmowerultra import GameFactory, RoomScene, Player
+
+from taz.game import Game
 
 
-class TestRemoteLawnMowerUltra(TestCase):
-    def test_string_match_is_looking_at_gnomes(self):
-        processor = CommandProcessor()
-        self.assertTrue(processor.is_looking_at_gnomes("look at gnome"))
-        self.assertTrue(processor.is_looking_at_gnomes("look at gnomes"))
-        self.assertTrue(processor.is_looking_at_gnomes("look gnomes"))
+class PlayerTestCase(TestCase):
 
-    def test_string_match_is_looking_at_tools(self):
-        processor = CommandProcessor()
-        self.assertTrue(processor.is_looking_at_tools("look at tools"))
-        self.assertTrue(processor.is_looking_at_tools("look at pile of tools"))
-        self.assertTrue(processor.is_looking_at_tools("look tool"))
-        self.assertTrue(processor.is_looking_at_tools("look at tool"))
-        self.assertTrue(processor.is_looking_at_tools("look at pile"))
-        self.assertTrue(processor.is_looking_at_tools("look pile"))
+    def test_player_has_room_id(self):
+        player = Player()
+        self.assertIsNone(player.room_id)
 
+
+class GameFactoryTestCase(TestCase):
+
+    def setUp(self):
+        self.mock_stdout = StringIO()
+        self.mock_stdin = StringIO()
+        self.mock_stdin.write("some stuff\n")
+        self.mock_stdin.seek(0)
+        self.game = GameFactory(self.mock_stdin, self.mock_stdout, {
+            "config": {"starting-room": "room"},
+            "rooms": [{
+                "name": "room",
+                "description": "This is test room",
+                "objects": dict()
+            }]}).create()
+
+    def test_new_game_should_have_one_scene(self):
+        self.assertEqual(len(self.game.registered_scenes), 1)
+
+    def test_game_has_room_scene(self):
+        self.assertEqual(self.game.get_name_of_top_scene(), "room")
+
+    def test_game_has_file_like_object_in_render_context(self):
+        fob = self.game.render_context["output_fob"]
+        message = "hello world\n"
+        fob.write(message)
+        self.assertIn(message, fob.getvalue())
+
+    def test_game_has_file_like_object_in_update_context(self):
+        fob = self.game.update_context["input_fob"]
+        self.assertTrue(hasattr(fob, "readline"))
+        self.assertEqual("some stuff\n", fob.readline())
+
+    def test_update_context_contains_world_data(self):
+        world_data = self.game.update_context["world_data"]
+        self.assertEqual(len(world_data["rooms"]), 1)
+        first_room = world_data["rooms"][0]
+        self.assertIn("name", first_room)
+        self.assertIn("objects", first_room)
+
+    def test_should_put_a_player_instance_on_the_room_scene(self):
+        self.assertIsNotNone(self.game.registered_scenes["room"].player)
+
+    def test_game_factory_raises_if_missing_starting_room(self):
+        with self.assertRaises(GameFactory.MissingStartingRoom):
+            game = GameFactory(None, None, dict()).create()
+
+
+class RoomSceneTestCase(TestCase):
+
+    def setUp(self):
+        self.scene = RoomScene("room")
+        self.stdout = StringIO()
+        self.context = {
+            "output_fob": self.stdout 
+        }
+        self.scene.update({
+            "input_fob": StringIO()
+        })
+        self.scene.output_buffer.write("line one\nline two")
+        self.scene.output_buffer.seek(0)
+
+    def test_render_should_write_to_output_buffer(self):
+        self.scene.render(self.context)
+        self.assertEqual("line one\nline two", self.stdout.getvalue())
+
+    def test_output_buffer_should_not_accumulate(self):
+        self.scene.render(self.context)
+        self.scene.update({
+            "input_fob": StringIO()
+        })
+        self.assertEqual(0, len(self.scene.output_buffer.getvalue()))
+
+
+class RoomSceneLookTestCase(TestCase):
+
+    def setUp(self):
+        self.scene = RoomScene("room")
+        self.scene.player = Player()
+        self.scene.player.room_id = "room"
+        self.stdin = StringIO()        
+        self.stdout = StringIO()
+        self.update_context = {
+            "input_fob": self.stdin,
+            "world_data": {
+                "config": {"starting-room": "room"},
+                "rooms": [{
+                    "name": "room",
+                    "description": "This is test room",
+                    "objects": {
+                        "Unit Test": "It appears... useful"
+                    }
+                }]}
+        }
+        self.render_context = {
+            "output_fob": self.stdout            
+        }
+
+    def process_command(self, command):
+        self.stdin.write(command + "\n")
+        self.stdin.seek(0)
+        self.scene.update(self.update_context)    
+        self.scene.render(self.render_context)
+        self.stdin = StringIO()
+
+    def assertOutputContains(self, message):
+        self.assertIn(message, self.stdout.getvalue())
+
+    @skip("TODO")
+    def test_no_good_command(self):
+        pass
+
+    def test_look(self):
+        self.process_command("look")
+        self.assertOutputContains("This is test room")
+
+    def test_look_at(self):
+        self.process_command("look at Unit Test")
+        self.assertOutputContains("It appears... useful")
+
+    @skip("TODO")
+    def test_look_at_nothing(self):
+        pass
